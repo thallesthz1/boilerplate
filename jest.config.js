a6deeb63446c3849dce26378595d411e6c0f1d6c
Thalles <thalles.colaco@escolaconquer.com.br>
2023-06-22 14:00:04 -0300

    setting up project

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,13 @@
-module.exports = {
-  testEnvironment: 'jsdom',
-  testPathIgnorePatterns: ['/node_modules/', '/.next/'],
-  collectCoverage: true,
-  collectCoverageFrom: ['src/**/*.ts(x)'],
-  setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts']
+// jest.config.js
+const nextJest = require('next/jest')
+
+const createJestConfig = nextJest({
+  dir: './'
+})
+
+/** @type {import('jest').Config} */
+const customJestConfig = {
+  moduleDirectories: ['node_modules', '<rootDir>/'],
+  testEnvironment: 'jest-environment-jsdom'
 }
+module.exports = createJestConfig(customJestConfig)
